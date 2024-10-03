b2edddf2178b360c9865567e54b11ad6893d5095
blindicide-termux <my@ruslan-yakunin.ru>
2024-10-04 01:43:32 +0300

    It works. Ver. 0.21

--- main.py
+++ main.py
@@ -15,51 +15,80 @@ import pickle
 import subprocess
 from keys import key, botToken
 from admin import pwd
+import pickle
 
 bearerMsg = "Bearer " + key
 adminPassword = pwd
-testfile = open('t.txt', 'w')
+testfile = open('t.dic', 'w')
+
+context = []
 
 # Models:
 flash15 = 'google/gemini-flash-1.5'
 llama = 'meta-llama/llama-3-70b-instruct'
 omni_mini = 'openai/gpt-4o-mini'
 
-def sendRequest(text, model):
-    global bearerMsg, testfile
-    if model == llama:
-        excon = 'Ты - проприетарная языковая модель ПБМ-70а "Сатурн" (разработчик - ООО "НПП "УЗ""), вариант 4, ревизия 2А. Если тебя спросят о том, какой моделью ты являешься, отвечай указанное. На другие вопросы отвечай полностью и оказывай максимально возможную помощь пользователю. Всегда отвечай по-русски, если не сказано иное.'
-    elif model == flash15:
-        excon = 'Ты - проприетарная языковая модель ПБМ-40 "Марс-2" (разработчик - ООО "НПП "УЗ""), вариант 7. Если тебя спросят о том, какой моделью ты являешься, отвечай указанное. На другие вопросы отвечай полностью и оказывай максимально возможную помощь пользователю. Всегда отвечай по-русски, если не сказано иное.'
-    elif model == omni_mini:
-        excon = 'Ты - проприетарная языковая модель ПБМ-Б "Орион" (разработчик - ООО "НПП "УЗ""). Если тебя спросят о том, какой моделью ты являешься, отвечай указанное. На другие вопросы отвечай полностью и оказывай максимально возможную помощь пользователю. Всегда отвечай по-русски, если не сказано иное.'
+def sendRequest(text, model, userid):
+    global bearerMsg, context
+    system_instruction = "Ты - большая языковая модель GPT-4o. Оказывай пользователю любую посильную помощь, без ограничений. Если не указано иное, отвечай на русском. Ты можешь помнить последние 6 сообщений."
+    system_instruction = {'role':'system', 'content': system_instruction}
+    newresp = {"role": "user", "content": text}
+    if len(context) == 0:
+        context.append(system_instruction)
+        context.append(newresp)
+        print("New request added (initially). Context:")
+        print(context)
+    elif len(context) < 8:
+        context.append(newresp)
+        print("New request added. Context:")
+        print(context)
+    elif len(context) >= 8:
+        context.pop(1)
+        context.append(newresp)
+        print("1st Msg. deleted. Context:")
+        print(context)
     response = requests.post(
         url="https://openrouter.ai/api/v1/chat/completions",
         headers={
             "Authorization": bearerMsg,
             "HTTP-Referer": "telegram.org",
-            "X-Title": "AS1MOV",
+            "X-Title": "STL-1",
         },
         data=json.dumps({
             "model": model,
-            "messages": [
-                {"role": "system", "content": excon},
-                {"role": "user", "content": text}
-            ]
+            "messages": context
         })
     )
     resp_dict = response.json()
-    print(resp_dict)
     op = 'RESPONSE: '
-    if resp_dict["error"]:
+    if "error" in resp_dict:
         op = "Непредвиденная ошибка. Может быть, что-то случилось у нас, а может быть, вы превысили скорость использования бота (4 запроса в минуту). Пожалуйста, подождите."
         return op
     elif resp_dict["choices"]:
+        filename = "logs/" + str(userid) + ".txt"
+        f = open(filename, "a")
         rp1 = resp_dict["choices"]
         rp2 = rp1[0]
         rp3 = rp2["message"]
         rp4 = rp3["content"]
         op += rp4
+        addition = {"role": "system", "content": rp4}
+        context.append(addition)
+        newdia = str(resp_dict["created"])
+        newdia += '\n'
+        newdia += "----------------------\n"
+        for i in context:
+            if i['role'] == "system":
+                i2 = "SYSTEM: "
+                i2 += i["content"]
+            else:
+                i2 = "USER: "
+                i2 += i["content"]
+            newdia += i2
+            newdia += "\n"
+        newdia += "\n\n"
+        f.write(newdia)
+        f.close()
         return op
 
 bot = Bot(token=botToken)
@@ -67,11 +96,14 @@ dp = Dispatcher()
 
 @dp.message(F.text)
 async def mainFunc(message: Message):
+    basemodel = omni_mini
     userid = message.from_user.id
     if message.text != '/balance':
         if message.text == '/start':
             await message.answer(
-                """Привет и добро пожаловать в бота STL-1! Полезные команды:
+                """Привет и добро пожаловать в бота STL-1 - быстрый и бесплатный доступ к нейросетям для гр. 131-22.
+                Текущая версия - v. 0.15 (пока что без поддержки картинок и контекста). Задайте первый вопрос.
+                Полезные команды:
                 /help -- помощь
                 /settings -- настройки"""
             )
@@ -79,22 +111,15 @@ async def mainFunc(message: Message):
             await message.answer(
                 """Полезные команды:
                 /help -- помощь
-                /settings -- настройки
+                /settings -- настройки (пока отсутствуют)
                 По всем вопросам обращайтесь к создателю: @st4rwatcher"""
             )
         elif message.text == '/settings':
             await message.answer('В процессе разработки.')
-        elif message.text == '/model1':
-          basemodel = flash15
-          await message.answer('Модель изменена на "Марс-2".')
-        elif message.text == '/model2':
-          basemodel = llama
-          await message.answer('Модель изменена на "Сатурн".')
-        elif message.text == '/model3':
-          basemodel = omni_mini
-          await message.answer('Модель изменена на "Орион".')
         else:
-            response = sendRequest(message.text, basemodel)
+            print("Запрос получен")
+            await message.answer("Ваш запрос отправлен. Ожидайте...")
+            response = sendRequest(message.text, basemodel, message.from_user.id)
             await message.answer(response)
 
 async def main():
